327f1b05f664d50715a1452453f173fe98d26097
Anna Lawley and paul Hess <pauljacksonhess@gmail.com>
2016-01-12 08:42:24 -0800

    add click to remove functionality to responses

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
   $("button#hello").click(function() {
-    $("ul").prepend("<li>Hello!</li>");
+    $("ul#user").prepend("<li>Hello!</li>");
+    $("ul#webpage").prepend("<li>Hello back!</li>");
+
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 
   $("button#goodbye").click(function() {
